ea9a8145e614e57498713860b5ca13895f5f5157
saianlord <iansacr12@gmail.com>
2024-11-25 18:37:36 -0600

    First commit

---
+++ public/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (event) => {
+    setTimeout(() => {
+        form.reset()
+    }, 1000);
+});
